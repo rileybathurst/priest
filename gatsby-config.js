c0f6f194b1de1535b55a7f202ebcb6b684db2070
Riley Bathurst <riley@rileybathurst.com>
2021-05-08 12:47:32 -0700

    linode strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1338",
+        apiURL: "45.79.101.19:1338",
         contentTypes: [
           "service",
           `galleries`,
