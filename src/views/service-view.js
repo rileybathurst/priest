8f0ddd48c411a43f83ea3a8cba24cf24b3c4c7fe
Riley Bathurst <riley@rileybathurst.com>
2021-06-13 18:09:26 -0700

    show words for now

--- src/views/service-view.js
+++ src/views/service-view.js
@@ -84,6 +84,7 @@ const ServiceView = ({ service }) => {
                 {service.gallery.map((photos) => (
                     <li key={photos.hash} className="blocks-gallery-item">
                       <p>{photos.formats.thumbnail.id}</p>
+                      <p>{photos.hash}</p>
                     {/* <li>test{photos.formats.medium.id}</li> */}
                     {/* I have had issues with null values this was fixed with removing and adding images */}
                     {/* check it with localhost:8000/___graphql */}
@@ -95,7 +96,7 @@ const ServiceView = ({ service }) => {
                       /> 
                       theres an image cache issue with this */}
                       {/* <GatsbyImage image={image} alt={photos.hash} /> */}
-                      <GatsbyImage image={photos.formats.thumbnail.childImageSharp.gatsbyImageData} alt={photos.hash} /> 
+                      {/* <GatsbyImage image={photos.formats.thumbnail.childImageSharp.gatsbyImageData} alt={photos.hash} />  */}
                     </li>
                 ))}
               </ul>
