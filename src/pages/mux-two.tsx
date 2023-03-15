8be8b07a1c6d28999685b13427e79ca6803eb209
Riley <riley@rileybathurst.com>
2023-03-15 16:32:42 -0700

    phone test

---
+++ src/pages/mux-two.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useRef } from "react"
+
+import Layout from "../components/layout";
+
+import MuxPlayer from '@mux/mux-player-react';
+
+function MuxHome() {
+  // const [isVisible, setIsVisible] = useState(document.hidden)
+  const ref = useRef();
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState !== "visible") {
+      let vid = ref.current;
+      vid.removeAttribute('loop');
+      vid.classList.remove("noco");
+    }
+  })
+
+  return (
+    <MuxPlayer
+      streamType="on-demand"
+      playbackId="KX8WTyujaU9laFVeyL020000y8jDcyXgdLCXJc1G01mYXac"
+      autoPlay="muted"
+      loop
+      noHotKeys
+      ref={ref}
+      className="noco"
+    // TODO: needs a poster image
+    />
+  )
+}
+
+const MuxPage = () => {
+
+  return (
+    <Layout>
+      <h1>Mux One</h1>
+      <MuxHome />
+      Test
+    </Layout>
+  )
+}
+
+export default MuxPage
---
+++ src/pages/visible.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef } from "react"
+
+import Layout from "../components/layout";
+
+function UsePageVisibility() {
+  const [isVisible, setIsVisible] = useState(document.hidden)
+
+  console.log(isVisible);
+
+  const ref = useRef();
+  // console.log(ref);
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "visible") {
+      console.log("🍔");
+      // setIsVisible(document.hidden);
+    } else {
+      console.log("🍟");
+      // setIsVisible(document.hidden);
+      // vid.removeAttribute(loop);
+
+      let vid = ref.current;
+      // console.log(vid);
+      // console.log(vid.attributes);
+      vid.removeAttribute('loop');
+
+
+    }
+  });
+
+  return (
+    <video
+      loop
+      ref={ref}
+    />
+  );
+
+}
+
+const VisiblePage = () => {
+  return (
+    <Layout>
+      <h1>Visible</h1>
+      Test
+      {/* <GetBrowserVisibilityProp /> */}
+      <UsePageVisibility />
+    </Layout >
+  )
+}
+
+export default VisiblePage
