beaa636b57ee5fd80f439f848d9ff9b34bda6225
Riley <riley@rileybathurst.com>
2023-11-16 09:51:42 -0800

    flip it

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -18,6 +18,57 @@ const TestPage = () => {
             // console.log(value);
 
             const third = Object.entries(value).map(([key, value]) => {
+              // console.log(key);
+              // console.log(value); // this is an object with keys are the numbers
+              // console.log(value[0]); // this is null because I need the key not the value
+              // console log the key of each value in the object
+              // console.log(Object.keys(value)); // this is an array of the keys
+
+              let reference = ['1.6', '2', '3', '4', '5', '6', '10', '12', '16', '20', '25', '32', '60', '80', '100', '120']
+              console.log(reference);
+
+              if (reference.includes(Object.keys(value))) {
+                // console.log('🦄');
+              } else {
+                // console.log('🦖');
+              }
+
+              // console.log(value); // this is an object with keys are the numbers
+              // create an array from the keys of object value
+              const keysArray = Object.keys(value);
+              console.log(keysArray); // ['key1', 'key2', 'key3']
+
+              const listItems = keysArray.map((key) => {
+                if (reference.includes(key)) {
+                  return (
+                    <li key={key} className="y">
+                      ✓ {key}
+                    </li>
+                  );
+                } else {
+                  return (
+                    <li key={key} className="n">
+                      ✗ {key}
+                    </li>
+                  );
+                }
+              });
+
+              /*               if (reference.includes(Object.keys(value))) {
+                              // console.log('🦄');
+                              return (
+                                <li key={value} className="yes">
+                                  {value}
+                                </li>
+                              )
+                            } else {
+                              // console.log('🦖');
+                              return (
+                                <li key={value} className="no">
+                                  &nbsp;
+                                </li>
+                              )
+                            } */
 
               let fourth = Object.entries(value).map(([key, value]) => {
                 // console.log(key);
@@ -32,34 +83,34 @@ const TestPage = () => {
 
                 // ! this is checking against key not value
                 // if (key in reference) {
-                if (reference.includes(key)) {
+                /* if (reference.includes(key)) {
                   return (
                     <li key={key} className="yes">
                       {key}
                     </li>
                   )
                 } else {
-
+ 
                   // console.log('🦖 no ' + key + ' is not in the array')
-
+ 
                   return (
                     <li key={key} className="no">
                       {key}
                     </li>
                   )
-                }
+                } */
               });
 
               // this is close but I might have to do it backwards to get spaces
               let fourV2 = Object.entries(value).map((value) => {
                 // console.log(value);
-                console.log(value[0]);
+                // console.log(value[0]);
 
                 // make them strings not numbers as thats what im getting from the yaml
                 let reference = ['1.6', '2', '3', '4', '5', '6', '10', '12', '16', '20', '25', '32', '60', '80', '100', '120']
-                console.log(reference);
+                // console.log(reference);
 
-                if (reference.includes(value[0])) {
+                /* if (reference.includes(value[0])) {
                   return (
                     <li key={value[0]} className="yes">
                       {value[0]}
@@ -71,7 +122,10 @@ const TestPage = () => {
                       {value[0]}
                     </li>
                   )
-                }
+                } */
+
+
+
               });
 
               return (
@@ -80,6 +134,7 @@ const TestPage = () => {
                   <ul className="thickness">
                     {/* {fourth} */}
                     {fourV2}
+                    {listItems}
                   </ul>
                 </>
               )
@@ -87,9 +142,9 @@ const TestPage = () => {
 
             return (
               <ul>
-                <li key={key}>
-                  {third}
-                </li>
+                {/* <li key={key}> */}
+                {third}
+                {/* </li> */}
               </ul>
             )
           });
